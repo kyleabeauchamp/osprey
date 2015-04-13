38b16f461bb24669a5f5c2297d94c3816e2b54a8
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-04-13 10:32:22 -0400

    Added backports

--- osprey/fit_estimator.py
+++ osprey/fit_estimator.py
@@ -7,7 +7,8 @@ import numpy as np
 import sklearn
 from sklearn.base import is_classifier, clone
 from sklearn.metrics.scorer import check_scoring
-from sklearn.utils.validation import check_arrays
+#from sklearn.utils.validation import check_arrays
+from msmbuilder.utils import check_arrays
 from sklearn.externals.joblib import Parallel, delayed
 from sklearn.cross_validation import _check_cv as check_cv, _safe_split, _score
 
